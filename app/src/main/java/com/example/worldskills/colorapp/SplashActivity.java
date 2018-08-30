bcad054f2f2523328a816fba9c67b93fad17b1c0
Brahiam Musse <bamusse@misena.edu.co>
2018-08-30 11:58:20 -0500

    1

--- app/src/main/java/com/example/worldskills/colorapp/SplashActivity.java
+++ app/src/main/java/com/example/worldskills/colorapp/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.example.worldskills.colorapp;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        CountDownTimer cambiar = new CountDownTimer(2000,2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                //Intent menu = new Intent(SplashActivity.this)
+            }
+        }.start();
     }
 }
