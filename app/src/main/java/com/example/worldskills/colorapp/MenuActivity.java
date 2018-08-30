d319c5811962eacdd8809e5ccbb74eda2924c8a0
Brahiam Musse <bamusse@misena.edu.co>
2018-08-30 14:03:37 -0500

    3

--- app/src/main/java/com/example/worldskills/colorapp/MenuActivity.java
+++ app/src/main/java/com/example/worldskills/colorapp/MenuActivity.java
@@ -1,13 +1,38 @@
 package com.example.worldskills.colorapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    //Se declara variables
+    Button btnJugar, btnConfiguracion, btnPuntajes;
+    TextView tvMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //Se instancia variables
+        btnConfiguracion = (Button)findViewById(R.id.btnConfiguracion;
+        btnJugar = (Button)findViewById(R.id.btnJugar);
+        btnPuntajes = (Button)findViewById(R.id.btnPuntajes);
+        tvMenu = (TextView) findViewById(R.id.tvMenu);
+
+        Typeface fuentes = Typeface.createFromAsset(getAssets(),"font/brlnsdb.ttf");
+        tvMenu.setTypeface(fuentes);
+
+        btnPuntajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent puntajes = new Intent(MenuActivity.this,)
+            }
+        });
     }
 }
