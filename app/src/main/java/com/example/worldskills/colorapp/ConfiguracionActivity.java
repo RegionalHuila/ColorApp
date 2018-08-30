c39c1bdee9fd785143c9b102c8e8400b559686e8
Brahiam Musse <bamusse@misena.edu.co>
2018-08-30 14:56:15 -0500

    4

--- app/src/main/java/com/example/worldskills/colorapp/ConfiguracionActivity.java
+++ app/src/main/java/com/example/worldskills/colorapp/ConfiguracionActivity.java
@@ -2,12 +2,53 @@ package com.example.worldskills.colorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class ConfiguracionActivity extends AppCompatActivity {
+
+    Spinner spOpciones;
+    EditText etTiempoPalabra,etTiempoJuego,etNumeroIntentos;
+    TextView ocultarIntentos,ocultarTiempoJuego;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+
+        spOpciones = (Spinner) findViewById(R.id.spOpciones);
+        etNumeroIntentos = (EditText)findViewById(R.id.etNumeroIntentos);
+        etTiempoJuego = (EditText)findViewById(R.id.etTiempoJuego);
+        etTiempoPalabra = (EditText)findViewById(R.id.etTiempoPalabra);
+        ocultarIntentos = (TextView)findViewById(R.id.ocultarIntentos);
+        ocultarTiempoJuego = (TextView)findViewById(R.id.ocultarTiempoJuego);
+
+
+        String[] tipoJuego = {"Tiempo", "Intentos"};
+         ArrayAdapter<String> arrayAdapter = ArrayAdapter.createFromResource(this, tipoJuego, android.R.layout.simple_list_item_1);
+         spOpciones.setAdapter(arrayAdapter);
+
+
+      spOpciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+          @Override
+          public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              if (spOpciones.getSelectedItem().equals("Tiempo")){
+
+              }
+          }
+
+          @Override
+          public void onNothingSelected(AdapterView<?> parent) {
+
+          }
+      });
+
     }
 }
--- app/src/main/java/com/example/worldskills/colorapp/MenuActivity.java
+++ app/src/main/java/com/example/worldskills/colorapp/MenuActivity.java
@@ -20,18 +20,33 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         //Se instancia variables
-        btnConfiguracion = (Button)findViewById(R.id.btnConfiguracion;
+        btnConfiguracion = (Button)findViewById(R.id.btnConfiguracion);
         btnJugar = (Button)findViewById(R.id.btnJugar);
         btnPuntajes = (Button)findViewById(R.id.btnPuntajes);
         tvMenu = (TextView) findViewById(R.id.tvMenu);
 
-        Typeface fuentes = Typeface.createFromAsset(getAssets(),"font/brlnsdb.ttf");
+        Typeface fuentes = Typeface.createFromAsset(getAssets(),"assets/brlnsdb.ttf");
         tvMenu.setTypeface(fuentes);
 
         btnPuntajes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent puntajes = new Intent(MenuActivity.this,)
+                Intent puntajes = new Intent(MenuActivity.this,PuntajesActivity.class);
+                startActivity(puntajes);
+            }
+        });
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent jugar = new Intent(MenuActivity.this,PuntajesActivity.class);
+                startActivity(jugar);
+            }
+        });
+        btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent jugar = new Intent(MenuActivity.this,ConfiguracionActivity.class);
+                startActivity(jugar);
             }
         });
     }
